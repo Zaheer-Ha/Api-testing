81b4f7756ed924724e9056f537abf7f732e28d39
Zaheer <92054435+Zaheer-Ha@users.noreply.github.com>
2023-11-26 23:54:17 +0530

    Quote Iteration

--- src/App.js
+++ src/App.js
@@ -1,50 +1,57 @@
-import {useState} from "react";
-
+import React, { useState } from 'react';
 
 export default function App() {
-const [advice, setAdvice] = useState("");
-const [text, setConsole] = useState("");
+  const [advice, setAdvice] = useState('');
+  const [text, setText] = useState('');
 
+  async function getQuote() {
+    const res = await fetch('https://api.adviceslip.com/advice');
+    const data = await res.json();
+    setAdvice(data.slip.advice);
+  }
 
-async function getQuote() {
+  async function getChange() {
+    let newText = '';
 
-const res = await fetch("https://api.adviceslip.com/advice");
-const data = await res.json();
-setAdvice(data.slip.advice);
+    for (let i = 0; i < 10; i++) {
+      const timestamp = Date.now(); // Unique timestamp for each request
+      const res = await fetch(`https://api.adviceslip.com/advice?timestamp=${timestamp}`);
+      const data = await res.json();
+      newText += data.slip.advice + '\n';
+    }
 
-}
-
-async function getChange ()  {
-
-const tmp = await fetch("https://api.adviceslip.com/advice");
-var info = await tmp.json();
-
-setConsole(info.slip.advice);
-
-}
+    setText(newText);
+  }
 
   return (
-            <div>
-            <h1 style={{background: "red"}}>
-            {advice}
-            </h1>
+    <div>
+      <h1 style={{ background: 'red' }}>{advice}</h1>
 
-           
-            
-           <button onClick={getQuote} style= {{margin: "30px", 
-           height: "100px", width: "100px", 
-           background: "lightblue"}}> getQuote </button>
-           
-           
-            <h2>
-             {text}
-            </h2>
+      <button
+        onClick={getQuote}
+        style={{
+          margin: '30px',
+          height: '100px',
+          width: '1000px',
+          background: 'lightblue'
+        }}
+      >
+        getQuote
+      </button>
 
-            <button onClick={getChange}>
-              console
-            </button>
-            
-            </div>    
+      <h1 style={{ background: '#484443', color: '#f1f1f1' }}>{text}</h1>
 
+      <button
+        onClick={getChange}
+        style={{
+          margin: '30px',
+          height: '100px',
+          width: '1000px',
+          background: '1d232d'
+        }}
+      >
+        Double Quote
+      </button>
+    </div>
   );
 }
